bac154e606986a06374a93c530acc34580e10e33
m-salman-afzal <m.salmanafzal@hotmail.com>
2023-10-09 23:51:54 +0500

    fix

--- src/Components/GithubCardComponent.tsx
+++ src/Components/GithubCardComponent.tsx
@@ -1,4 +1,4 @@
-import {Card, Row, Col, Skeleton, Avatar, Tooltip} from "antd";
+import {Card, Row, Col, Skeleton, Tooltip} from "antd";
 
 import {ISetRepositories} from "../Containers/GithubSearchContainer";
 import {SEARCH_TYPES} from "../Utils/constants";
@@ -28,46 +28,54 @@ export const GithubCardComponent = (props: IGithubCardComponent) => {
             )}
 
             <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}} justify={"center"}>
-                {props.repositories.items.map((repository: any) => {
-                    return (
-                        <Col className="gutter-row" key={repository.id}>
-                            <Card
-                                key={repository.id}
-                                hoverable
-                                style={{width: 240, margin: 10}}
-                                loading={props.loading}
-                                cover={
-                                    <Image
-                                        alt="example"
-                                        src={`${
-                                            props.searchType === SEARCH_TYPES.REPOSITORIES
-                                                ? repository?.owner?.avatar_url
-                                                : repository?.avatar_url
-                                        }`}
-                                        loading="eager"
-                                        width={240}
-                                        height={240}
-                                    />
-                                }>
-                                {props.searchType === SEARCH_TYPES.REPOSITORIES && (
-                                    <Tooltip title={repository.description && repository.description}>
-                                        <p>
-                                            Description:{" "}
-                                            {repository.description
-                                                ? `${repository.description.slice(0, 30)}...`
-                                                : " NA"}
-                                        </p>
-                                        {!repository.description && <br />}
-                                    </Tooltip>
-                                )}
+                {props.repositories.items.map(
+                    (repository: {
+                        id: number;
+                        owner?: {avatar_url: string};
+                        avatar_url?: string;
+                        description?: string;
+                        html_url: string;
+                    }) => {
+                        return (
+                            <Col className="gutter-row" key={repository.id}>
+                                <Card
+                                    key={repository.id}
+                                    hoverable
+                                    style={{width: 240, margin: 10}}
+                                    loading={props.loading}
+                                    cover={
+                                        <Image
+                                            alt="example"
+                                            src={`${
+                                                props.searchType === SEARCH_TYPES.REPOSITORIES
+                                                    ? repository?.owner?.avatar_url
+                                                    : repository?.avatar_url
+                                            }`}
+                                            loading="eager"
+                                            width={240}
+                                            height={240}
+                                        />
+                                    }>
+                                    {props.searchType === SEARCH_TYPES.REPOSITORIES && (
+                                        <Tooltip title={repository.description && repository.description}>
+                                            <p>
+                                                Description:{" "}
+                                                {repository.description
+                                                    ? `${repository.description.slice(0, 30)}...`
+                                                    : " NA"}
+                                            </p>
+                                            {!repository.description && <br />}
+                                        </Tooltip>
+                                    )}
 
-                                <a href={`${repository.html_url}`} target="_blank">
-                                    Go to {props.searchType === SEARCH_TYPES.USERS ? "Profile" : "Repository"}
-                                </a>
-                            </Card>
-                        </Col>
-                    );
-                })}
+                                    <a href={`${repository.html_url}`} target="_blank">
+                                        Go to {props.searchType === SEARCH_TYPES.USERS ? "Profile" : "Repository"}
+                                    </a>
+                                </Card>
+                            </Col>
+                        );
+                    }
+                )}
             </Row>
         </>
     );
